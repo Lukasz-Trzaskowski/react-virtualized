224c33dc0a8dd47e806bc1c8156dee27357466f7
Lukasz <lukasz.trzaskowski@ubs.com>
2017-08-23 17:27:05 +0100

    #444 , see https://github.com/bvaughn/react-virtualized/issues/444

--- source/vendor/detectElementResize.js
+++ source/vendor/detectElementResize.js
@@ -50,15 +50,18 @@ export default function createDetectElementResize(nonce) {
 
     var resetTriggers = function(element) {
       var triggers = element.__resizeTriggers__,
-        expand = triggers.firstElementChild,
-        contract = triggers.lastElementChild,
-        expandChild = expand.firstElementChild;
-      contract.scrollLeft = contract.scrollWidth;
-      contract.scrollTop = contract.scrollHeight;
-      expandChild.style.width = expand.offsetWidth + 1 + "px";
-      expandChild.style.height = expand.offsetHeight + 1 + "px";
-      expand.scrollLeft = expand.scrollWidth;
-      expand.scrollTop = expand.scrollHeight;
+        expand = triggers.firstElementChild;
+
+      if (expand) {
+        var contract = triggers.lastElementChild,
+          expandChild = expand.firstElementChild;
+        contract.scrollLeft = contract.scrollWidth;
+        contract.scrollTop = contract.scrollHeight;
+        expandChild.style.width = expand.offsetWidth + 1 + "px";
+        expandChild.style.height = expand.offsetHeight + 1 + "px";
+        expand.scrollLeft = expand.scrollWidth;
+        expand.scrollTop = expand.scrollHeight;
+      }
     };
 
     var checkTriggers = function(element) {
